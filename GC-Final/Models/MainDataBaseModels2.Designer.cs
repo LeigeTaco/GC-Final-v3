61788a1a0158b91d2bdff53f2b3ace3216071ca1
LiegeTaco <ashketchup.pkmn2gmail.com>
2018-09-13 09:04:52 -0400

    Hoping real hard

--- GC-Final/Models/MainDataBaseModels2.Designer.cs
+++ GC-Final/Models/MainDataBaseModels2.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ashke\source\repos\GC-Final\GC-Final\Models\MainDataBaseModels.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ashke\source\repos\GC-Final-v3\GC-Final\Models\MainDataBaseModels.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
